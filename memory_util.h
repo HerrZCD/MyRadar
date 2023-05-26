293abad8fe0f4712454934ffaf2c13b76116bf69
Chundong Zhang <chuzhang@microsoft.com>
2023-05-26 15:49:43 +0800

    1

--- memory_util.h
+++ memory_util.h
@@ -2,17 +2,20 @@
 #define BASE_PROCESS_UTIL_H_
 
 #include <windows.h>
-
 #include <tlHelp32.h>
 
 namespace MemoryUtil {
-template <typename T> T RPM(HANDLE hProcess, DWORD address);
-template <typename T> T ReadMemory(DWORD address);
-template <typename T> void WriteMemory(DWORD address, T content);
-template <typename T> T* PointMemory(DWORD address);
-template <typename T> DWORD protectMemory(DWORD address, DWORD prot);
+template <typename T>
+T RPM(HANDLE hProcess, DWORD address);
+template <typename T>
+T ReadMemory(DWORD address);
+template <typename T>
+void WriteMemory(DWORD address, T content);
+template <typename T>
+T* PointMemory(DWORD address);
+template <typename T>
+DWORD protectMemory(DWORD address, DWORD prot);
+char* GetModuleBase(const wchar_t* ModuleName, DWORD procID);
 }  // namespace MemoryUtil
 
-
-
-#endif // BASE_PROCESS_UTIL_H_
+#endif  // BASE_PROCESS_UTIL_H_
